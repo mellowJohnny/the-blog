4003806ad92916e4cbeab6db7738aa2ed6317abc
mellowjohnny <ccouillard@gmail.com>
2021-02-12 21:15:46 -0500

    No major changes

--- main.js
+++ main.js
@@ -199,3 +199,30 @@ const executeSearch = () => {
 }
 
 $submit.click(executeSearch)
+
+/*
+AWS Lambda call - createBlogPost
+*/
+
+// define the callAPI function that takes the blog title, author and post body as parameters:
+let callBlogPostAPI = (title,author,postBody)=>{
+// instantiate a headers object
+let myHeaders = new Headers();
+// add content type header to object
+myHeaders.append("Content-Type", "application/json");
+// using built in JSON utility package turn object to string and store in a variable
+let raw = JSON.stringify({"title":title,"author":author,"postBody":postBody});
+// create a JSON object with parameters for API call and store in a variable
+let requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+  };
+  
+  // make API call to BlogPost endpoint with parameters and use promises to get response
+  fetch("https://s4ge5t9w06.execute-api.us-east-2.amazonaws.com/dev ", requestOptions)
+  .then(response => response.text())
+  .then(result => alert(JSON.parse(result).body))
+  .catch(error => console.log('error', error));
+        }
