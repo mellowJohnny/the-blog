2e0f3016ad49137161a7f47a4afb13b929c152e3
mellowjohnny <ccouillard@gmail.com>
2021-12-16 21:01:53 -0500

    fancier weather.js - added callback

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,17 +1,42 @@
+// Global Variables
+
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Get geolocation from the Browser, if available..
 if('geolocation' in navigator) {
     /* geolocation is available */
     navigator.geolocation.getCurrentPosition((position) => {
        const lat = position.coords.latitude;
        const long = position.coords.longitude;
-       console.log(`Latitude is ${lat}, Longitude is ${long}`);
-        sendGeo(lat,long);
+        // Our callback to getForcast()
+        getForecast(lat,long);
       });
   } else {
     /* geolocation IS NOT available */
   }
 
+  // Geolocation callback...does it work?
   function sendGeo(latitude,longitude) {
     const myLat = latitude;
     const myLong = longitude;
     console.log(`sendGeo says... ${myLat} & ${myLong}`);
+  }
+
+  // Possible callback candidate
+    getForecast(latitude,longitude) = async () => {
+    const myLat = latitude;
+    const myLong = longitude;
+    const urlToFetch = `${weatherUrl}?&lat=${myLat}&lon=${myLong}&units=metric&APPID=${openWeatherKey}`;
+    
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok){
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        return jsonResponse;
+      }
+    }
+    catch(error){
+      console.log(error);
+    }
   }
