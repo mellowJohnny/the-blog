5160c3b3827f422d957cbe2cd31b874347a30662
mellowjohnny <ccouillard@gmail.com>
2021-12-20 15:13:28 -0500

    comment out the return

--- scripts/weather.js
+++ scripts/weather.js
@@ -10,36 +10,36 @@ if('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition((position) => {
        lat = position.coords.latitude;
        long = position.coords.longitude;
-        // Our callback to getForcast()
+        // We got some coordinates, let's make a callback to getWeather()!
         getWeather(lat,long);
       });
   } else {
     /* geolocation IS NOT available */
   }
 
-  // Geolocation callback...
+  // This is our Geolocation callback...we call it asynchronously becasue we don't want tit o block page load
   async function getWeather(latitude,longitude) {
+
+    // Our Latitude & Longitude constants, passed to us 
     const myLat = latitude;
     const myLong = longitude;
+
+    // Go fetch the weather forcast based on the Lat & Long the browser gave us
+    // Note: we specifically fetch in Metric
     const urlToFetch = `${openWeatherUrl}?&lat=${myLat}&lon=${myLong}&units=metric&APPID=${openWeatherKey}`;
-    // Did we get the Lat & Long?
-    console.log(`Got Lat & Long...getWeather() says... ${myLat} & ${myLong}`)
 
     // Now that we have the latitude & longitude, let's get the weather forecast
     try {
       const response = await fetch(urlToFetch);
       if (response.ok){
+
         // This is our JSON Object containing the weather forecast
         const jsonResponse = await response.json();
-        // Stringify it just so we can colsole.log it
-        // const forecast = JSON.stringify(jsonResponse);
-        //console.log(`In getWeather() - here is the forcast... ${forecast}`);
-        
-        // Let's set up an Object to hold the forecast
-        // var forecast = JSON.parse(jsonResponse);
-        var temp = jsonResponse.main.temp;
+        // Step 1: Now that jsonResponse represents our forcast as a JSON Object
+        // let's get the values we want to display
+        const temp = jsonResponse.main.temp;
         console.log(`The current temp is ${temp} degrees`);
-        return jsonResponse;
+        // return jsonResponse;
       }
     }
     catch(error){
