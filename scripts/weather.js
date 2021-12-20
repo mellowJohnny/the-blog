ffef3c3ed01a0702cf49828299209eaac7586a09
mellowjohnny <ccouillard@gmail.com>
2021-12-20 10:24:46 -0500

    weather.js changes

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,5 +1,6 @@
 // Global Variables
 const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const openWeatherKey = '49f84d9cdb7907dfd2b02085e270372e';
 
 // Get geolocation from the Browser, if available..
 if('geolocation' in navigator) {
@@ -20,9 +21,8 @@ if('geolocation' in navigator) {
     const myLong = longitude;
     console.log(`getWeather says... ${myLat} & ${myLong}`)
 
+    fetchForecast;
     
-
-// The Big Lebowski...
     const fetchForecast = async () => {
         const urlToFetch = `${weatherUrl}?&lat=${myLat}&lon=${myLong}&units=metric&APPID=${openWeatherKey}`;
         
@@ -30,7 +30,7 @@ if('geolocation' in navigator) {
           const response = await fetch(urlToFetch);
           if (response.ok){
             const jsonResponse = await response.json();
-            console.log(jsonResponse);
+            console.log(`weather.js says... ${jsonResponse}`);
             return jsonResponse;
           }
         }
@@ -39,7 +39,7 @@ if('geolocation' in navigator) {
         }
       }
 
-      fetchForecast;
+      
   } 
 
   
