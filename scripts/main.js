d1c5264602dc6fca840cc56b7c8091a1423b0ca4
mellowjohnny <ccouillard@gmail.com>
2021-12-21 12:17:44 -0500

    Moved all Date manipulation  to helper functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 
-/** 
+/**
  * This is the main AWS call used to create a NEW BLOG POST
- * AWS Lambda call - createBlogPost
  * Called from the wlcms.html page
-*/
+ * @param {*} title 
+ * @param {*} author 
+ * @param {*} postBody 
+ */
 
-// define the callAPI function that takes the blog title, author and post body as parameters:
 function callBlogPostAPI (title,author,postBody){
   // instantiate a headers object
   let myHeaders = new Headers();
@@ -28,7 +29,7 @@ function callBlogPostAPI (title,author,postBody){
   .catch(error => console.log('error', error));
   }
 
-  /** 
+ /** 
     * This is the main AWS call used to FETCH ALL BLOG POSTS
     * AWS Lambda call - getBlogPost
     * Called from the index.html page
@@ -76,6 +77,14 @@ function callBlogPostAPI (title,author,postBody){
        });
       }
 
+/**
+ * Function to DISPLAY Blogs dynamically
+ * @param {*} postBody 
+ * @param {*} author 
+ * @param {*} date 
+ * @param {*} title 
+ */
+
    function addBlog(postBody, author, date, title) {
        // Populate the blogsDiv...
  
@@ -84,122 +93,129 @@ function callBlogPostAPI (title,author,postBody){
        const cleanTitle = JSON.parse(title);
        const cleanAuthor = JSON.parse(author);
        const cleanPostBody = JSON.parse(postBody);
+       // Pass the Date Object to our Magic Date fixer...
+       fixDate(date);
+        
 
-       // Magic Date fixing action...
-        function fixDate(date){
-            const d = new Date(date);
-            // Get the day of the week as an Interger, then convert it to the name
-            const dayNum = d.getDay();
-            const weekDay = getDayOfTheWeek(dayNum);
-
-            // Get the month as an Intger, convert it to the name
-            const monthNum = d.getMonth(); 
-            const month = getMonthName(monthNum);
-            // Get the year
-            const year = d.getFullYear(); 
-
-            // Gets the day of the month
-            const dateNum = d.getDate(); 
-
-            // Smoosh it all together and send it back...
-            return weekDay + ", " + month + " " + dateNum + ", " + year;
-        }
-
-/**
- * Helper function to get the day name, given an Integer value
- * @param {*} day 
- */
-        function getDayOfTheWeek(dayNum){
-
-            if (dayNum === 1){
-                dayName = "Monday";
-                return dayName;
-            }
-            if (dayNum === 2){
-                dayName = "Tuesday";
-                return dayName;
-            }
-            if (dayNum === 3){
-                dayName = "Wednesday";
-                return dayName;
-            }
-            if (dayNum === 4){
-                dayName = "Thursday";
-                return dayName;
-            }
-            if (dayNum === 5){
-                dayName = "Friday";
-                return dayName;
-            }
-            if (dayNum === 6){
-                dayName = "Saturday";
-                return dayName;
-            }
-            if (dayNum === 0){
-                dayName = "Sunday";
-                return dayName;
-            }
-        }
-/**
- * Helper function to get the month name, given an Integer value
- * @param {*} month 
- */
-        function getMonthName(monthNum){
-            if (monthNum === 0){
-                month = "January";
-                return month;
-            }
-            if (monthNum === 1){
-                month = "February";
-                return month;
-            }
-            if (monthNum === 2){
-                month = "March";
-                return month;
-            }
-            if (monthNum === 3){
-                month = "April";
-                return month;
-            }
-            if (monthNum === 4){
-                month = "May";
-                return month;
-            }
-            if (monthNum === 5){
-                month = "June";
-                return month;
-            }
-            if (monthNum === 6){
-                month = "July";
-                return month;
-            }
-            if (monthNum === 7){
-                month = "August";
-                return month;
-            }
-            if (monthNum === 8){
-                month = "September";
-                return month;
-            }
-            if (monthNum === 9){
-                month = "October";
-                return month;
-            }
-            if (monthNum === 10){
-                month = "November";
-                return month;
-            }
-            if (monthNum === 11){
-                month = "December";
-                return month;
-            }
-        }
- 
        // Setup a variable to hold the reference to our Div, 'cause we got work to do!
        var blogBody = document.getElementById("blogsDiv");
        blogBody.innerHTML += "<strong>" + cleanTitle + "</strong> <p> <strong>" + cleanAuthor + " </strong> <p> <strong>" + fixDate(date) + "</strong> <p>" + cleanPostBody + "<br /> <br />"; 
       
    }
 
-   
+   /**
+    * Fixes our Raw Date object coming back from the JSON response
+    * @param {*} date 
+    * @returns 
+    */
+
+   function fixDate(date){
+    const d = new Date(date);
+    // Get the day of the week as an Interger, then convert it to the name
+    const dayNum = d.getDay();
+    const weekDay = getDayOfTheWeek(dayNum);
+
+    // Get the month as an Intger, convert it to the name
+    const monthNum = d.getMonth(); 
+    const month = getMonthName(monthNum);
+    // Get the year
+    const year = d.getFullYear(); 
+
+    // Gets the day of the month
+    const dateNum = d.getDate(); 
+
+    // Smoosh it all together and send it back...
+    return weekDay + ", " + month + " " + dateNum + ", " + year;
+}
+
+   /**
+ * Helper function to get the month name, given an Integer value
+ * @param {*} month 
+ */
+    function getMonthName(monthNum){
+        if (monthNum === 0){
+            month = "January";
+            return month;
+        }
+        if (monthNum === 1){
+            month = "February";
+            return month;
+        }
+        if (monthNum === 2){
+            month = "March";
+            return month;
+        }
+        if (monthNum === 3){
+            month = "April";
+            return month;
+        }
+        if (monthNum === 4){
+            month = "May";
+            return month;
+        }
+        if (monthNum === 5){
+            month = "June";
+            return month;
+        }
+        if (monthNum === 6){
+            month = "July";
+            return month;
+        }
+        if (monthNum === 7){
+            month = "August";
+            return month;
+        }
+        if (monthNum === 8){
+            month = "September";
+            return month;
+        }
+        if (monthNum === 9){
+            month = "October";
+            return month;
+        }
+        if (monthNum === 10){
+            month = "November";
+            return month;
+        }
+        if (monthNum === 11){
+            month = "December";
+            return month;
+        }
+    }
+
+    /**
+ * Helper function to get the day name, given an Integer value
+ * @param {*} day 
+ */
+     function getDayOfTheWeek(dayNum){
+
+        if (dayNum === 1){
+            dayName = "Monday";
+            return dayName;
+        }
+        if (dayNum === 2){
+            dayName = "Tuesday";
+            return dayName;
+        }
+        if (dayNum === 3){
+            dayName = "Wednesday";
+            return dayName;
+        }
+        if (dayNum === 4){
+            dayName = "Thursday";
+            return dayName;
+        }
+        if (dayNum === 5){
+            dayName = "Friday";
+            return dayName;
+        }
+        if (dayNum === 6){
+            dayName = "Saturday";
+            return dayName;
+        }
+        if (dayNum === 0){
+            dayName = "Sunday";
+            return dayName;
+        }
+    }
   
