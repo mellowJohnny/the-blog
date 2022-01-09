1acce4d631d405d09f2eb5fd834301be6e1d3173
mellowjohnny <ccouillard@gmail.com>
2022-01-09 11:24:55 -0500

    Bug fix

--- scripts/weather.js
+++ scripts/weather.js
@@ -140,18 +140,7 @@ if('geolocation' in navigator) {
     return newTime;
   }
 
-/**
- * Function to send less weather info at specific breakpoint 
- * @param {*} x 
- */
 
-  function weatherBreak(x) {
-    if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
-    } else {
-      document.body.style.backgroundColor = "pink";
-    }
-  }
   
   var x = window.matchMedia("(max-width: 700px)")
   weatherBreak(x) // Call listener function at run time
