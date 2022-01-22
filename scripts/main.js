46fad33349b44ad38bdc14b346b642005c1156de
mellowjohnny <ccouillard@gmail.com>
2022-01-22 15:17:06 -0500

    Create Card Set Page

--- scripts/main.js
+++ scripts/main.js
@@ -32,6 +32,45 @@ function callBlogPostAPI (title,author,postBody,type){
   .catch(error => console.log('error', error));
   }
 
+
+/**
+ * This is the main AWS call used to CREATE a NEW CARDS POST
+ * Called from the wlcms.html page
+ * @param {*} setName 
+ * @param {*} mfg
+ * @param {*} year 
+ * @param {*} postBody 
+ */
+
+ function callCreateCardSetAPI (setName,year,postBody,mfg){
+    // instantiate a headers object
+    let myHeaders = new Headers();
+  
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+  
+    // using built in JSON utility package turn object to string and store in a variable
+    let raw = JSON.stringify({"setName":setName,"mfg":mfg,"year":year,"postBody":postBody});
+  
+    // create a JSON object with parameters for API call and store in a variable
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+      };
+    
+    // make API call to cardPost endpoint with parameters and use promises to get response
+    fetch("https://05uss9ffij.execute-api.us-east-2.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+    }
+
+
+
+
+
  /** 
     * This is the main AWS call used to FETCH ALL BLOG POSTS
     * AWS Lambda call - getBlogs end-point
