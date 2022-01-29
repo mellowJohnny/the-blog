4f647aed9bc15b292930618f3bacb22aca77c6df
mellowjohnny <ccouillard@gmail.com>
2022-01-28 20:25:03 -0500

    header

--- scripts/cardSets.js
+++ scripts/cardSets.js
@@ -87,12 +87,12 @@
                     <table class="set-details-table-style" border="1">
                         <tr>
                             <td style="width:500px"><strong>${cleanSetName}</strong></td> 
-                            <td rowspan="5" style="text-align:center">
-                                <img src="${cleanHeaderImg}" class="table-header-img"></img>
-                            </td>
                         </tr>
                         <tr>
                             <td><strong><i>Set Size:</i></strong> ${cleanSetSize} cards</td>
+                            <td rowspan="4" style="text-align:center">
+                                <img src="${cleanHeaderImg}" class="table-header-img"></img>
+                            </td>
                         </tr>
                         <tr>
                             <td><strong><i>Sub Sets & Inserts:</i></strong> ${cleanSubsets} </td>
