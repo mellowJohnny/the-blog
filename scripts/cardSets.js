50371c2cb875d6a087187a26f965ebad4db5524b
mellowjohnny <ccouillard@gmail.com>
2022-01-29 20:40:18 -0500

    space...I need my space

--- scripts/cardSets.js
+++ scripts/cardSets.js
@@ -89,6 +89,9 @@
                             <td style="width:400px;font-size:20px"><strong>${cleanSetName}</strong></td><td></td>
                         </tr>
                         <tr>
+                            <td></td>
+                        </tr>
+                        <tr>
                             <td><strong><i>Set Size:</i></strong> ${cleanSetSize} cards</td>
                             <td rowspan="5" style="text-align:center"><img src="${cleanHeaderImg}" class="table-header-img"></img></td>
                         </tr>
