82e24be4756401d555c4a1aa0f43fba9be2aeeae
mellowjohnny <ccouillard@gmail.com>
2022-03-05 14:57:03 -0500

    Passing Params to Lambda!

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,13 @@
 /** 
     * TEST AWS call 
-    * AWS Lambda call - getBlogPost
-    * Called from the index.html page
+    * AWS Lambda call 
+    * Called from the test.html page
   */
 
- function fetchAllBlogs() {
+ function fetchTest() {
 
     // Test endpoint
-    const urlToFetch = `https://kuefte6pgk.execute-api.us-east-2.amazonaws.com/dev`;
+    const urlToFetch = `https://c9kzoz1g13.execute-api.us-east-2.amazonaws.com/dev?year=2022`;
 
        
     fetch(urlToFetch)
@@ -41,10 +41,7 @@
                  * It gets called it once for each blog post, essentially populating each blog post one at a time
                  **/
                 for (var i = 0; i < blogPostArray.Items.length; i++) {
-                    displayBlog(blogPostArray.Items[i].postBody,
-                     blogPostArray.Items[i].author,
-                     blogPostArray.Items[i].time,
-                     blogPostArray.Items[i].title);
+                    displayBlog(blogPostArray.Items[i].year);
                  }
             }
         } 
@@ -57,150 +54,21 @@
  
  /**
   * Function to DISPLAY Blogs dynamically
-  * @param {*} postBody 
-  * @param {*} author 
-  * @param {*} date 
-  * @param {*} title 
+  * @param {*} year 
   */
  
-    function displayBlog(postBody, author, date, title) {
+    function displayBlog(year) {
         // Populate the blogsDiv...
   
         // Cleanup the JSON we get back so it's back to a String 
         // We parsed the first object we got back, but that didn't parse the contents of the inner properties
         // so we need to explicitly parse title, author, and the blog
-        const cleanTitle = JSON.parse(title);
-        const cleanAuthor = JSON.parse(author);
-        const cleanPostBody = JSON.parse(postBody);
- 
-        // Format the Date by passing it to our Magic Date fixer...
-        fixDate(date);
+        const cleanYear = JSON.parse(year);
          
         // Setup a variable to hold the reference to our Div, 'cause we got work to do!
         let blogBody = document.getElementById("testDiv");
         blogBody.innerHTML += 
                      `<p>
-                     <strong>${cleanTitle}</strong> <br>
-                     <i>${cleanAuthor} </i><br>
-                     <i>${fixDate(date)}</i> <br><br>
-                     ${cleanPostBody} 
+                     <strong>The Year is: ${cleanYear}</strong> <br>
                      </p> <hr/> `;
     }
- 
-    // ----------------------------- Date Helper Functions ----------------------------
- 
-    /**
-     * Fixes our Raw Date object coming back from the JSON response
-     * @param {*} date 
-     * @returns 
-     */
- 
-    function fixDate(date){
-     const d = new Date(date);
-     // Get the day of the week as an Interger, then convert it to the name
-     const dayNum = d.getDay();
-     const weekDay = getDayOfTheWeek(dayNum);
- 
-     // Get the month as an Intger, convert it to the name
-     const monthNum = d.getMonth(); 
-     const month = getMonthName(monthNum);
-     // Get the year
-     const year = d.getFullYear(); 
- 
-     // Gets the day of the month
-     const dateNum = d.getDate(); 
- 
-     // Smoosh it all together and send it back...
-     return weekDay + ", " + month + " " + dateNum + ", " + year;
- }
- 
-    /**
-  * Helper function to get the month name, given an Integer value
-  * @param {*} month 
-  */
-     function getMonthName(monthNum){
-         if (monthNum === 0){
-             month = "January";
-             return month;
-         }
-         if (monthNum === 1){
-             month = "February";
-             return month;
-         }
-         if (monthNum === 2){
-             month = "March";
-             return month;
-         }
-         if (monthNum === 3){
-             month = "April";
-             return month;
-         }
-         if (monthNum === 4){
-             month = "May";
-             return month;
-         }
-         if (monthNum === 5){
-             month = "June";
-             return month;
-         }
-         if (monthNum === 6){
-             month = "July";
-             return month;
-         }
-         if (monthNum === 7){
-             month = "August";
-             return month;
-         }
-         if (monthNum === 8){
-             month = "September";
-             return month;
-         }
-         if (monthNum === 9){
-             month = "October";
-             return month;
-         }
-         if (monthNum === 10){
-             month = "November";
-             return month;
-         }
-         if (monthNum === 11){
-             month = "December";
-             return month;
-         }
-     }
- 
-     /**
-  * Helper function to get the day name, given an Integer value
-  * @param {*} day 
-  */
-      function getDayOfTheWeek(dayNum){
- 
-         if (dayNum === 1){
-             dayName = "Monday";
-             return dayName;
-         }
-         if (dayNum === 2){
-             dayName = "Tuesday";
-             return dayName;
-         }
-         if (dayNum === 3){
-             dayName = "Wednesday";
-             return dayName;
-         }
-         if (dayNum === 4){
-             dayName = "Thursday";
-             return dayName;
-         }
-         if (dayNum === 5){
-             dayName = "Friday";
-             return dayName;
-         }
-         if (dayNum === 6){
-             dayName = "Saturday";
-             return dayName;
-         }
-         if (dayNum === 0){
-             dayName = "Sunday";
-             return dayName;
-         }
-     }
