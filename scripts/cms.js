2c0e19e8b4e79a4011d5f0c1141b21c9b722a19d
mellowjohnny <ccouillard@gmail.com>
2022-03-23 16:33:35 -0400

    setID param passing test

--- scripts/cms.js
+++ scripts/cms.js
@@ -170,6 +170,70 @@ function displayCardSets(setID, setName) {
                `;
 }
 
+/** 
+ * This function fetches a single card set, given it's ID
+ * 
+ * **/
+
+function fetchCardSetsByID(id) {
+  // Set up a global variable to hold the API URL
+  const urlToFetch = `https://.execute-api.us-east-2.amazonaws.com/dev?setID=${id}`;
+        
+  fetch(urlToFetch)
+     .then(function (response) {
+         const jsonResponse = response.json();
+         return jsonResponse; // Our Promise object
+     })
+     .then(function (data) {
+     // 'data' is an Object at this point...this is basically the record set returned bt dynamoDB
+     // First let's return an array of the object's properties
+         const returnedData = Object.entries(data); 
+
+     // Next let's just get the 'body' property returned by the Lambda call
+        for (const [key, value] of returnedData) {
+            if (key === "body"){
+
+          // Now that we have the 'body' key, we need to convert the value (currently a JSON String) to a JSON Object 
+          // so that we can pull out the properties of each blog post 
+          const cardSetArray = JSON.parse(value);
+
+          // Check to see if we have any results...    
+          if (cardSetArray.Items.length === 0) {
+
+              // No results...return a friendly message
+              let blogBody = document.getElementById("editSetDiv");
+              blogBody.innerHTML = `...these aren't the Droids you're looking for...`;
+
+              // If we have no results, stop processing
+              return;
+          }
+
+          // Now that the data we got back is a JSON object, let's loop over all the Posts...
+          // The 'Items' property holds an array of all the set reviews 
+          // Let's loop through that array and display the fields we want!
+          // We call the displayBlog() function to control the display, calling it once
+          // for each set review, essentially populating each review one at a time
+
+             for (var i = 0; i < cardSetArray.Items.length; i++) {
+                 editCardSet(
+                  cardSetArray.Items[i].setID,
+                  cardSetArray.Items[i].setName);
+              }
+          }
+      }
+
+     })
+     .catch(function (err) {
+         // Error...return a friendly message
+         let blogBody = document.getElementById("editSetDiv");
+         blogBody.innerHTML = `...Ah, Houston, we've had a problem...`;
+         console.log('Something went wrong...: ' + err);
+     });
+}
+}
+
+
+
 
 
 
