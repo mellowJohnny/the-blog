fbebaeb0f69366f769532aad2ea7573af447b795
mellowjohnny <ccouillard@gmail.com>
2022-03-26 11:24:44 -0400

    Fix

--- scripts/cms.js
+++ scripts/cms.js
@@ -326,7 +326,7 @@ function populateCardSet(postBody,year,mfg,size,subsets,stars,formats,setName) {
 
 function updateCardSet(setName,size,subsets,stars,formats,year,postBody,mfg) {
     // Let's change the colour of the button, now that we've clicked it...
-    const btn = document.getElementById('cmsSubmitButton').style.backgroundColor = "#36a5e6";
+    document.getElementById('cmsSubmitButton').style.backgroundColor = "#36a5e6";
 
     // instantiate a headers object
     let myHeaders = new Headers();
