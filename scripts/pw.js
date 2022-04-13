984a5c053ea9c18fd984391784ba3459b5f0ff96
mellowJohnny <30263225+mellowJohnny@users.noreply.github.com>
2022-04-12 22:11:53 -0400

    PW function fix

--- scripts/pw.js
+++ scripts/pw.js
@@ -1,8 +1,23 @@
-function pwGenerator(length) {
+function pwGenerator_old(length) {
     let pwLength = length;
     let pw = Math.random().toString(pwLength).slice(2);
     
     // Return some HTML 
     let pwDiv = document.getElementById("pwDiv");
     pwDiv.innerHTML += `<h1>${pw}</h1> `;
+}
+
+function pwGenerator(length) {
+    let pw  = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	
+   // Loop "length" number of times, rndomly picking a character from the array
+    for ( var i = 0; i < length; i++ ) {
+      pw += characters.charAt(Math.floor(Math.random() * 58)); //58 is the number of letters / numbers array
+   }
+   
+   // Return some HTML 
+    let pwDiv = document.getElementById("pwDiv");
+    pwDiv.innerHTML += `<h1>${pw}</h1> `;
+
 }
