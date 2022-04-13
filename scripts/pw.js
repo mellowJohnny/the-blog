61bf531edb0337d2d50c0f1f3489fd68f389bc3c
mellowjohnny <ccouillard@gmail.com>
2022-04-13 15:17:36 -0400

    no idea

--- scripts/pw.js
+++ scripts/pw.js
@@ -55,17 +55,4 @@ function randomString(length, chars) {
     app.innerHTML += `<h1>${result}</h1> `;
 }
 
-function pwGenerator(length) {
-    let pw  = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	
-   // Loop "length" number of times, rndomly picking a character from the array
-    for ( var i = 0; i < length; i++ ) {
-      pw += characters.charAt(Math.floor(Math.random() * 58)); //58 is the number of letters / numbers array
-   }
-   
-   // Return some HTML 
-    let pwDiv = document.getElementById("pwDiv");
-    pwDiv.innerHTML += `<h1>${pw}</h1> `;
 
-}
