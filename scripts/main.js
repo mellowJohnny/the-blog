cab7f982e20b733a8fb9bf8c108eadbe676a4451
mellowJohnny <ccouillard@gmail.com>
2023-05-19 14:06:28 -0400

    Adding Tech page to global nav function

--- scripts/main.js
+++ scripts/main.js
@@ -958,5 +958,25 @@ function fetchNav(pageName){
       </table>
         `;
     }
+
+    else if (pageName === "tech") {
+        nav.innerHTML = `
+        <table class="top-nav">
+        <tr>
+          <td class="nav-td"><a href="/index.html">Home</a> </td>
+          <td class="nav-td"><a href="/waxReviews.html?year=1987&pageName=junkWax">90s Hockey Junk Wax</a> </td>
+          <td class="nav-td"><a href="waxReviews.html?year=1979&pageName=classicWax">Classic 80s Hockey Sets</a> </td>
+          <td class="nav-td">
+            <div class="dropdown">
+              <span>Mustang Mach-E</span>
+                <div class="dropdown-content">
+                  <a href="/ev.html?blogType=3">Mach-E Blog</a> <br><br>
+                  <a href="/ev.html?blogType=4">Power-Up Software Updates</a> 
+                </div>
+            </div>
+          </td>
+        </tr>
+      </table>`;
+    }
     
 }
