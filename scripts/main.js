540d5dacf1952a08a00ee03c6d49e9a5af333bbd
mellowJohnny <ccouillard@gmail.com>
2023-09-07 16:22:36 -0400

    debug

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ var globalPageName = "";
            const returnedData = Object.entries(data); 
 
            // DEBUG
-           console.log(returnedData.count);
+           console.log(returnedData);
          
        // Next let's just get the 'body' property returned by the Lambda call
           for (const [key, value] of returnedData) {
