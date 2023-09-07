6be316ec6304cecce5c110790a7581006e92fff6
mellowJohnny <ccouillard@gmail.com>
2023-09-07 16:59:43 -0400

    Removing deeBug

--- scripts/main.js
+++ scripts/main.js
@@ -38,8 +38,8 @@ var globalPageName = "";
                const blogPostObject = JSON.parse(value);
 
                // DEBUG
-               console.log(`There are ${blogPostObject.Count} blogs returned...`);
-               console.log(`The 2nd post is ${blogPostObject.Items[1].postBody}`);
+              // console.log(`There are ${blogPostObject.Count} blogs returned...`);
+              // console.log(`The 2nd post is ${blogPostObject.Items[1].postBody}`);
              
             // Next all getSortOrder, passing the attribute we want to sort on. Remember, "Items" is the array of JSON-formatted blogPosts
             // If the blogType passed in from the page is 3 (mach-e blogs) lets display oldest blogs first, for everything else, order newest first 
