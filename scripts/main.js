9a7cd4bb89d76bfd09a3a5e8420ac564eb456f3e
mellowJohnny <ccouillard@gmail.com>
2023-09-07 16:07:20 -0400

    debigin'

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,9 @@ var globalPageName = "";
        // 'data' is an Object at this point...this is basically the record set returned bt dynamoDB
        // First let's return an array of the object's properties
            const returnedData = Object.entries(data); 
+
+           // DEBUG
+           console.log(returnedData);
          
        // Next let's just get the 'body' property returned by the Lambda call
           for (const [key, value] of returnedData) {
