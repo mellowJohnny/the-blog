ca1c640495f77db04b75cc4aa06461f7e8fa5402
mellowJohnny <ccouillard@gmail.com>
2025-12-16 13:35:36 -0500

    adding new year to timmines set picker

--- scripts/helper.js
+++ scripts/helper.js
@@ -316,7 +316,7 @@ function renderSetPicker(year) {
   const ranges = [
     { start: 1979, end: 1986, className: "classic-set-nav-td", pageName: "classicWax" },
     { start: 1987, end: 1993, className: "junk-set-nav-td", pageName: "junkWax" },
-    { start: 2015, end: 2024, className: "junk-set-nav-td", pageName: "timmies" }
+    { start: 2015, end: 2026, className: "junk-set-nav-td", pageName: "timmies" }
   ];
 
   // Find which range the year belongs to
