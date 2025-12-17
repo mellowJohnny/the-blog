a681ff717fe2d7dea2230b718d71840b2ca2e08c
mellowJohnny <ccouillard@gmail.com>
2025-12-16 21:56:59 -0500

    passing BlogID in updateBlogPost

--- scripts/cms.js
+++ scripts/cms.js
@@ -510,6 +510,7 @@ function displayStagedBlogs(title, blogID) {
                     blogArray.Items[i].published,
                     blogArray.Items[i].blogType,
                     blogArray.Items[i].time,
+                    blogArray.Items[i].blogID,
                     blogArray.Items[i].title);
                 }
             }
