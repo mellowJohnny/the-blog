4a5c76abb214f42c28007df2de1dbc43a914ea05
mellowJohnny <ccouillard@gmail.com>
2026-02-01 11:21:27 -0500

    Removing lazy-loading from images

--- scripts/wax.js
+++ scripts/wax.js
@@ -135,7 +135,7 @@ function renderCardSetPage() {
                     <strong>${setName}</strong>
                 </td>
                 <td rowspan="7" style="width: 75%; text-align: center;">
-                    <img src="${headerImg}${headerImgName}" class="table-header-img" loading="lazy">
+                    <img src="${headerImg}${headerImgName}" class="table-header-img">
                 </td>
             </tr>
 
@@ -179,7 +179,7 @@ function renderCardSetPage() {
             </tr>
             <tr>
                 <td style="text-align:center">
-                    <img src="${footerImg}${footerImgName}" class="table-footer-img" loading="lazy">
+                    <img src="${footerImg}${footerImgName}" class="table-footer-img">
                 </td>
             </tr>
         </table>
