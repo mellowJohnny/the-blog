b9a8971f9b706cd6a1171ca67a4018df549b08dc
mellowJohnny <ccouillard@gmail.com>
2026-09-22 22:30:09 -0400

    Adding 2019 Timmies to nav link

--- scripts/helper.js
+++ scripts/helper.js
@@ -389,7 +389,7 @@ const NAV_ITEMS = {
   home: { label: "Home", href: "/index.html" },
   junk: { label: "90s Junk Wax", href: "/waxReviews.html?year=1987&pageName=junkWax&blogCat=reg" },
   classic: { label: "Classic 80s Sets", href: "/waxReviews.html?year=1981&pageName=classicWax&blogCat=reg" },
-  timmies: { label: "Tim Hortons Hockey", href: "/waxReviews.html?year=2020&pageName=timmies&blogCat=tims" },
+  timmies: { label: "Tim Hortons Hockey", href: "/waxReviews.html?year=2019&pageName=timmies&blogCat=tims" },
   mcd: { label: "McDonald's Hockey", href: "/waxReviews.html?year=1991&pageName=mcd&blogCat=mcd" },
   tech: { label: "Tech", href: "/tech.html?blogType=1&pageName=tech" },
   mache: { label: "Mustang Mach-E", href: "/tech.html?blogType=3&pageName=ev" },
